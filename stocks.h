c3be37db416bd9f4efd71255368c3e5b24fd8a0d
Brian Stout <brianjonstout@gmail.com>
2017-02-04 14:53:38 -0500

    Function comments.

--- stocks.h
+++ stocks.h
@@ -10,9 +10,24 @@
 
 #include "tree.h"
 
+/** file_input() takes lines from a file and puts them through input_validation()
+*/
 tree * file_input(tree * root, FILE * fp);
+
+/** user_input() takes lines from stdin and puts them through input_validation()
+*/
 tree * user_input(tree * root);
+
+/** input_validation() is a function which receives ticker update lines and parses them for bad
+*       information.  If the information is bad it spits out the error and returns the passed tree
+*       unmodified.  If the input is good it splits it up in to the correct data types and passes.
+*       that data to the insert function and returns the modified tree.
+*/
 tree * input_validation(tree * root, char *buf);
+
+/** is_symbol_bad() checks the first word of the line passed by file_input() or user_input() and
+*       checks to see if it's a valid symbol.  It lets input_validation() know if it should quit out.
+*/
 bool is_symbol_bad(char *symbol);
 
 #endif
