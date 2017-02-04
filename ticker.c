144d7785b22e12f1d9169e0634660f6f7c8cdaea
Brian Stout <brianjonstout@gmail.com>
2017-02-04 13:40:15 -0500

    AVL's are working.

--- ticker.c
+++ ticker.c
@@ -11,12 +11,71 @@ typedef struct tree
     signed long cents;
     struct tree *left;
     struct tree *right;
+    int height;
     bool visited;
 }   tree;
 
-int symbol_compare(tree *root, char *word)
+int symbol_compare(char *word, tree *root)
 {
     return strncmp(word, root->symbol, strlen(word));
+}
+
+int height(tree * root)
+{
+    if (root != NULL)
+    {
+        return root->height;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int max(int a, int b)
+{
+    if (a > b) { return a; }
+    else { return b; }
+}
+
+tree * right_rotate(tree * root)
+{
+    tree * x = root->left;
+    tree * tmp = x->right;
+
+    x->right = root;
+    root->left = tmp;
+
+    root->height = max(height(root->left), height(root->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    return x;
+}
+
+tree * left_rotate(tree * root)
+{
+    tree * y = root->right;
+    tree * tmp = y->left;
+
+    y->left = root;
+    root->left = tmp;
+
+    root->height = max(height(root->left), height(root->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    return y;
+}
+
+int get_balance(tree * root)
+{
+    if (root != NULL)
+    {
+        return height(root->left) - height(root->right);
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 struct tree *insert(tree * root, char *symbol, char *name, signed long cents)
@@ -37,12 +96,13 @@ struct tree *insert(tree * root, char *symbol, char *name, signed long cents)
             root->cents = cents;
             root->left = root->right = NULL;
             root->visited = false;
+            root->height = 1;
         }
     }
     else
     {
         int cmpValue = 0;
-        cmpValue = symbol_compare(root, symbol);
+        cmpValue = symbol_compare(symbol, root);
 
         if (cmpValue == 0)
         {
@@ -54,7 +114,7 @@ struct tree *insert(tree * root, char *symbol, char *name, signed long cents)
                 free(symbol);
             }
         }
-        else if (cmpValue < 1)
+        else if (cmpValue < 0)
         {
             root->left = insert(root->left, symbol, name, cents);
         }
@@ -63,6 +123,31 @@ struct tree *insert(tree * root, char *symbol, char *name, signed long cents)
             root->right = insert(root->right, symbol, name, cents);
         }
     }
+    int balance = get_balance(root);
+
+    //Zig Zig
+    if (balance > 1 && (symbol_compare(symbol, root->left) < 0))
+    {
+        return right_rotate(root);
+    }
+    //Zag Zag
+    if (balance < -1 && (symbol_compare(symbol, root->right) > 0))
+    {
+        return left_rotate(root);
+    }
+    //Zig Zag
+    if (balance > 1 && (symbol_compare(symbol, root->left) > 0))
+    {
+        root->left = left_rotate(root->left);
+        return right_rotate(root);
+    }
+    //Zag Zig
+    if (balance < -1 && (symbol_compare(symbol, root->right) < 0))
+    {
+        root->right = right_rotate(root->right);
+        return left_rotate(root);
+    }
+
     return root;
 }
 
@@ -296,6 +381,10 @@ tree * user_input(tree * root)
         }
         else
         {
+            if(buf[0] == '#')
+            {
+                continue;
+            }
             buf[strlen(buf) - 1] = '\0';
             root = input_validation(root, buf);
         }
